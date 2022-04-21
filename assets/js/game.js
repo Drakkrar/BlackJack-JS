a9a84c9104e8faebe0edf9bb4bfbc47958821c25
Francisco Ramos <francisco.rsepulveda@alumnos.udg.mx>
2022-04-21 01:51:09 -0500

    Ligeras modificaciones y modulacion del codigo de JS para posteriormente pulir el codigo y comenzar a trabajar con clases

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,139 +5,151 @@
  * (2S) = Two of Spades (Espadas)
  **/
 
-let deck            = [];
-let playerDeck      = [];
-let computerDeck    = [];
-let cardType        = ['C', 'D', 'H', 'S'];
-let highCard        = ['A', 'J', 'Q', 'K'];
-let playerPoints    = 0;
-let computerPoints  = 0;
-let isHumanPlaying  = true;
+(() => {
+    'use strict'
 
-const btnNewGame  = document.querySelector('#btn-newgame');
-const btnGetCard  = document.querySelector('#btn-getcard');
-const btnStop     = document.querySelector('#btn-stop');
-const txtPlayerScore = document.querySelector('#player-score');
-const txtComputerScore = document.querySelector('#computer-score');
-const cardsPlayer = document.querySelector('#player-cards');
-const cardsComputer = document.querySelector('#computer-cards');
 
-// Invoca la baraja y crea la referencia a esta llenando la variable 'deck' declarada arriba.
-const doNewDeck = () => {
-    
-    for( let i = 2; i <= 10; i++ ) {
-        for ( let type of cardType ) {
-            deck.push( i + type);
+    let deck            = [],
+        playerDeck      = [],
+        computerDeck    = [];
+    const cardType        = ['C', 'D', 'H', 'S'],
+          highCard        = ['A', 'J', 'Q', 'K'];
+    let playerPoints    = 0,
+        computerPoints  = 0;
+
+    // Referencias HTML
+    const btnNewGame  = document.querySelector('#btn-newgame'),
+          btnGetCard  = document.querySelector('#btn-getcard'),
+          btnStop     = document.querySelector('#btn-stop'),
+          txtPlayerScore = document.querySelector('#player-score'),
+          txtComputerScore = document.querySelector('#computer-score'),
+          cardsPlayer = document.querySelector('#player-cards'),
+          cardsComputer = document.querySelector('#computer-cards');
+
+    // Invoca la baraja y crea la referencia a esta llenando la variable 'deck' declarada arriba.
+    const doNewDeck = () => {
+        if (deck.length !== 0) {
+            deck = [];
+        }
+
+        for( let i = 2; i <= 10; i++ ) {
+            for ( let type of cardType ) {
+                deck.push( i + type);
+            }
+        }
+
+        for ( let high of highCard) {
+            for (let type of cardType ) {
+                deck.push( high + type )
+            }
+        }
+        deck = _.shuffle(deck);
+    }
+
+    // Retorna la ultima carta del arreglo y lo elimina de este.
+    const getCardFromDeck = () => {
+        // TODO Cambiar 'NoCard' por el manejador de errores.
+        return deck.length === 0 ? 'NoCard' : deck.pop();
+    }
+
+    // Calcular el valor segun string
+    const getCardValue = ( card ) => {
+        const value = card.substring( 0, card.length - 1 );
+        // Verifica si el valor no es un numero, si lo es:
+            // Verifica si es una A, si lo es retorna 11, si no, 10.
+        // Si es un numero, retorna el valor multiplicado a 1 (por que java script lo dice).
+        return isNaN(value) ? ( value === 'A' ? 11 : 10 ) : value * 1;
+        
+    }
+
+    // Actualiza el valor del puntaje de la computadora/jugador
+    const updateScore = (participantScore, card, wichContainer) => {
+        this.participantScore = participantScore + getCardValue(card);
+        wichContainer.innerText = this.participantScore;
+        return this.participantScore;
+    }
+
+    // Entrega una carta y actualiza el DOM.
+    const giveCard = ( participantArray, card, wichContainer ) => {
+        const imgCard = document.createElement('img');
+        participantArray.push( card );
+
+        imgCard.src = `./assets/deck/${ card }.png`;
+        imgCard.classList.add('card');
+
+        wichContainer.append(imgCard);
+    }
+
+
+    // Turno de la computadora
+    const computerDoTurn = ( minPoints ) => {
+        let whoWin;
+        do {
+            const card = getCardFromDeck();
+            computerPoints = updateScore(computerPoints, card, txtComputerScore);
+            giveCard(computerDeck, card, cardsComputer)
+
+        } while ( (computerPoints < minPoints) && (minPoints <= 21) );
+        whoWin = getWinner();
+    } 
+
+    // Muestra quien es el ganador. Crea un array de un objeto con dos propiedades, ordena quien es el mayor y de esta manera sabe cual es el ganador.
+    const getWinner = () => {
+        // Esto solo funciona si son 2 jugadores, se necesitan modificaciones para funcionar con más
+        let playersScoreArr = [{ "entity": "player", "score": playerPoints }, { "entity": "computer", "score": computerPoints }];
+        playerScoreArr = playersScoreArr.sort();
+        return playersScoreArr[1].score === playersScoreArr[0].score ? 'draw' : playersScoreArr[1].score <= 21 ? playersScoreArr[1].entity : playersScoreArr[0].entity;
+    }
+
+    const disablePlayButtons = () => {
+        if (btnGetCard.disabled && btnStop.disabled){
+            btnGetCard.disabled = false;
+            btnStop.disabled    = false;
+        }else {
+            btnGetCard.disabled = true;
+            btnStop.disabled    = true;
         }
     }
 
-    for ( let high of highCard) {
-        for (let type of cardType ) {
-            deck.push( high + type )
-        }
-    }
-}
-
-// Retorna la ultima carta del arreglo y lo elimina de este.
-const getCardFromDeck = () => {
-    // TODO Cambiar 'NoCard' por el manejador de errores.
-    return deck.length === 0 ? 'NoCard' : deck.pop();;
-}
-
-// Calcular el valor segun string
-const getCardValue = ( card ) => {
-    const value = card.substring( 0, card.length - 1 );
-    // Verifica si el valor no es un numero, si lo es:
-        // Verifica si es una A, si lo es retorna 11, si no, 10.
-    // Si es un numero, retorna el valor multiplicado a 1 (por que java script lo dice).
-    return isNaN(value) ? ( value === 'A' ? 11 : 10 ) : value * 1;
-    
-}
-
-const giveCard = ( participantArray, card, wichContainer ) => {
-    const imgCard = document.createElement('img');
-    participantArray.push( card );
-
-    imgCard.src = `./assets/deck/${ card }.png`;
-    imgCard.classList.add('card');
-
-    wichContainer.append(imgCard);
-}
-const updateScore = (participantScore, card, wichContainer) => {
-    this.participantScore = participantScore + getCardValue(card);
-    wichContainer.innerText = this.participantScore;
-    return this.participantScore;
-}
-
-
-// Turno de la computadora
-const computerDoTurn = ( minPoints ) => {
-    let whoWin;
-    do {
+    //// Eventos
+    btnGetCard.addEventListener('click', () => {
         const card = getCardFromDeck();
-        computerPoints = updateScore(computerPoints, card, txtComputerScore);
-        giveCard(computerDeck, card, cardsComputer)
+        
+        playerPoints = updateScore(playerPoints, card, txtPlayerScore);
+        giveCard(playerDeck, card, cardsPlayer);
+        
+        
+        if ( playerPoints > 21 ) {
+            disablePlayButtons();
+            computerDoTurn(playerPoints);
+        } else if ( playerPoints === 21 ){
+            disablePlayButtons();
+            computerDoTurn(playerPoints);
+        }
+    });
 
-    } while ( (computerPoints < minPoints) && (minPoints <= 21) );
-    whoWin = getWinner();
-    console.log(whoWin);
-} 
-
-const playGame = () => {
-    // doNewDeck();
-    // deck = _.shuffle(deck);
-    
-
-    for (let i = 0; i < 2; i++) {
-
-
-    }
-
-}
-doNewDeck();
-deck = _.shuffle(deck);
-
-const getWinner = () => {
-    // Esto solo funciona si son 2 jugadores, se necesitan modificaciones para funcionar con más
-    let playersScoreArr = [{ "entity": "player", "score": playerPoints }, { "entity": "computer", "score": computerPoints }];
-    playerScoreArr = playersScoreArr.sort();
-    console.table(playersScoreArr)
-    return playersScoreArr[1].score === playersScoreArr[0].score ? 'draw' : playersScoreArr[1].score <= 21 ? playersScoreArr[1].entity : playersScoreArr[0].entity;
-}
-
-//// Eventos
-btnGetCard.addEventListener('click', () => {
-    const card = getCardFromDeck();
-
-    playerPoints = updateScore(playerPoints, card, txtPlayerScore);
-    // playerPoints +=  getCardValue(card);
-    // txtPlayerScore.innerText = playerPoints;
-    console.log(playerPoints, card);
-
-    // cardsPlayer.append(imgCard);
-
-    giveCard(playerDeck, card, cardsPlayer);
-
-
-    if ( playerPoints > 21 ) {
-        console.warn(' Ya perdiste.')
-        btnGetCard.disabled = true;
-        btnStop.disabled    = true;
-        isHumanPlaying = false;                 /// Pensado para despues.
+    btnStop.addEventListener('click', () => {
+        //TODO Agregar box de confirmacion.
+        disablePlayButtons();
         computerDoTurn(playerPoints);
-    } else if ( playerPoints === 21 ){
-        console.warn('Llegaste a 21.')
-        btnGetCard.disabled = true;
-        btnStop.disabled    = true;
-        isHumanPlaying = false;                 /// Pensado para despues.
-        computerDoTurn(playerPoints);
-    }
-});
+    });
 
-btnStop.addEventListener('click', () => {
-    //TODO Agregar box de confirmacion.
-    btnGetCard.disabled = true;
-    btnStop.disabled    = true;
-    isHumanPlaying      = false;                 /// Pensado para despues.
-    computerDoTurn(playerPoints);
-});
+    btnNewGame.addEventListener('click', () => {
+        //TODO agregar box de confirmacion.
+        doNewDeck();
+        deck = _.shuffle(deck);
+        disablePlayButtons();
+        playerPoints = 0;
+        computerPoints = 0;
+        txtPlayerScore.innerText = 0;
+        txtComputerScore.innerText = 0;
+        
+        cardsPlayer.innerHTML = '';
+        cardsComputer.innerHTML = '';
+    });
+
+    doNewDeck();
+
+})();
+
+
